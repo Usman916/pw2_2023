2db2c0e848f33b8be503c38e5a0b6a464daeadd8
Usman <114470735+Usman916@users.noreply.github.com>
2023-03-06 22:02:55 +0700

    Add files via upload

---
+++ pertemuan1/praktik/praktik2/variable.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo $_SERVER["DOCUMENT_ROOT"];
+
+
+    /*  Variable user
+        Variable yg di buat oleh user
+    */
+    echo "<br><br>";
+    $name = 'Man';
+    $name = 'Usman';
+    echo "Nama saya adalah  $name"; 
+
+
+?>
